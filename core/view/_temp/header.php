45dca2e05a858b509304eb141940ba9c81218053
ArtificialOR <ArtificialORIGIN@live.com>
2014-11-26 22:19:20 +0800

    Update source files

---
+++ core/view/_temp/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <meta charset=utf-8>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>PHP_MVC_sample</title>
+        
+        <link rel="stylesheet" href="<?php echo _URL_; ?>/html/.css/reset.css" />
+        <link rel="stylesheet" href="<?php echo _URL_; ?>/html/.css/style.css" />
+        
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+        </header>
